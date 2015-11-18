0b500943298ff4ce8dcd6b183972a9574bdaef36
Tim Park <timsjpark@gmail.com>
2015-11-18 16:58:28 -0700

    Remove css element test in remove_employee_spec
    * I don't think we should focus on the style elements of our app
      until we've nailed down at least a few key functionalities
    
    [#108135714]

--- spec/features/remove_employees_spec.rb
+++ spec/features/remove_employees_spec.rb
@@ -19,10 +19,6 @@ feature 'Removing employees' do
       expect(page).to have_content(employee.employee_number)
       expect(page).to have_content(employee.hire_date)
       expect(page).to have_content(employee.removal_date)
-      expect(page).to have_css('div.removed')
-      # This last line will test that a css class called 'removed'
-      # is included in a div tag. This is probably how I would modify
-      # the employees table to show that a specific employee was removed.
     end
   end
 end
