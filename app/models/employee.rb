c70ba06ffb7b5c8538a66ae67c4e5074b0c99e5a
Tim Park <timsjpark@gmail.com>
2015-11-19 09:05:41 -0700

    Change employee email validation
    [#108136018]

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,4 @@
 class Employee < ActiveRecord::Base
   validates_presence_of :first_name, :last_name, :email
-  validates :email, format: { with: /\A\w+@\w+\.\w+\z/ }
+  validates :email, format: { with: /@/ }
 end
