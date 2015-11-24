4ab4bb0cff1f59b1e91565a4e613a2343f12a7ea
Tim Park <timsjpark@gmail.com>
2015-11-23 18:42:04 -0700

    Add current_employee method
    [#108686632]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_employee
+
+  private
+
+  def current_employee
+    @employee ||= Employee.find(session[:id]) if session[:id]
+  end
 end
