d363798fd637ce1b1fbea9285455a6cf49c21f03
Danny Debevec <daniel.debevec@gmail.com>
2015-11-24 15:08:49 -0700

    Added sessions create method  *Added post route for login
    [#108686632]

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def login
   end
+
+  def create
+    employee = Employee.find_by_email(params[:email])
+    if employee && employee.authenticate(params[:password])
+      session[:id] = employee.id
+      redirect_to root_path,
+        notice: "Welcome back #{employee.first_name.capitalize}"
+    else
+      flash[:error] = 'Invalid email or password'
+      render :login
+    end
+  end
 end
