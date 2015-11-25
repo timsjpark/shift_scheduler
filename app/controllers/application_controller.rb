9c98dddfa16dffd1a0ad35e2f36a4f0981b5c263
Tim Park <timsjpark@gmail.com>
2015-11-24 21:34:56 -0700

    Change default cancancan method for current_ability
    * Add custom method in application controller to account for our
      application using an employee model vs. a user model
    
    [#108688516]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,9 @@ class ApplicationController < ActionController::Base
   def current_employee
     @employee ||= Employee.find(session[:id]) if session[:id]
   end
+
+  # change the default cancancan method to account for employee vs. user model
+  def current_ability
+    @current_ability ||= Ability.new(current_employee)
+  end
 end
