59afce1a9e08ec17cb03a977eb2f9cdd51845280
Tim Park <timsjpark@gmail.com>
2015-11-24 22:33:57 -0700

    Change current_ability method so it's no longer private
    [#108688516]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,14 +5,14 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_employee
 
+  # change the default cancancan method to account for employee vs. user model
+  def current_ability
+    @current_ability ||= Ability.new(current_employee)
+  end
+
   private
 
   def current_employee
     @employee ||= Employee.find(session[:id]) if session[:id]
-  end
-
-  # change the default cancancan method to account for employee vs. user model
-  def current_ability
-    @current_ability ||= Ability.new(current_employee)
   end
 end
