efe6dd9faff854d600165edb0b8bf860b990aa4e
Tim Park <timsjpark@gmail.com>
2015-12-01 17:38:48 -0700

    Create schedule and shift associations in Employee model
    [#109315784]

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,7 @@
 class Employee < ActiveRecord::Base
+  has_one :schedule
+  has_many :shifts, through: :schedule
+
   has_secure_password
   validates_presence_of :first_name, :last_name, :email
   validates :email, format: { with: /@/ }
