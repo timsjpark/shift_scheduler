af28eb2a7a64d45decd433e6a9500bc783002663
Tim Park <timsjpark@gmail.com>
2015-12-02 00:07:34 -0700

    Do some magical shift assignment stuff
    * Create method to find employee name for a particular shift
    
    * Change new shift form so that shifts can be assigned to different
      employees based on name

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,8 @@
 class Schedule < ActiveRecord::Base
   belongs_to :employee
+
+  def employee_name
+    employee = Employee.find_by_id(employee_id)
+    "#{employee.first_name} #{employee.last_name}"
+  end
 end
