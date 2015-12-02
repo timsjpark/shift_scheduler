801296abd71776acbce8f02768a3513185300d2b
Tim Park <timsjpark@gmail.com>
2015-12-02 01:58:13 -0700

    Update user permissions
    * Add load_and_authorize_resources to controllers
    
    * Modify ability method

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,4 +1,6 @@
 class SchedulesController < ApplicationController
+  load_and_authorize_resource
+
   def show
     @employee = Employee.find(params[:id])
     @shifts = Shift.where(schedule_id: params[:id])
--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,4 +1,6 @@
 class ShiftsController < ApplicationController
+  load_and_authorize_resource
+
   before_action :set_shift, only: [:show, :edit, :update, :destroy]
 
   # GET /shifts
