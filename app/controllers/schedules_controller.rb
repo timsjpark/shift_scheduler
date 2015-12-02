9f5d44a2f9a2e33856cf559749256dad498c532c
Tim Park <timsjpark@gmail.com>
2015-12-02 01:49:36 -0700

    Fix Schedule show to only display one employees shift

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,7 +1,7 @@
 class SchedulesController < ApplicationController
   def show
     @employee = Employee.find(params[:id])
-    @shifts = Shift.all.where(:schedule_id == params[:id])
+    @shifts = Shift.where(schedule_id: params[:id])
   end
 
   def set_employee
