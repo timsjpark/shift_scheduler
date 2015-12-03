e265322404e2899d36229acb0bea87a4793442d9
Tim Park <timsjpark@gmail.com>
2015-12-03 14:50:56 -0700

    Set callback route to sessions#create

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   get 'login' => 'sessions#login', as: 'login'
   post 'login' => 'sessions#create'
+  get "/auth/:provider/callback" => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
 
 end
