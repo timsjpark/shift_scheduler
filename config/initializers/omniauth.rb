407c72185be421bc84614da8cf0fef3608714a7f
Tim Park <timsjpark@gmail.com>
2015-12-03 14:46:21 -0700

    Revise scope of omniauth initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_auth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], {
-               scope: ['calendar',
+               scope: ['email',
                'https://www.googleapis.com/auth/calendar'],
                access_type: 'offline'}
 end
