b471ad2b5732e17e100250ae637ea0b3d8b50db8
Tim Park <timsjpark@gmail.com>
2015-12-03 15:18:09 -0700

    Configure oauth login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,12 +14,38 @@ class SessionsController < ApplicationController
     end
   end
 
+  def oauth
+    @employee = Employee.where(
+                            email: omniauth_options[:email]
+    ).first_or_initialize(omniauth_options)
+    if @employee.persisted?
+      session[:id] = @employee.id
+      redirect_to root_path,
+          notice: "Logged in as #{@employee.first_name.titleize}."
+    else
+      render 'employees/new'
+    end
+  end
+
   def destroy
     if employee = current_employee
       session[:id] = nil
       redirect_to root_path,
         notice: "#{employee.email} has been logged out"
 
+    end
+  end
+
+  private
+  def omniauth_options
+    if auth_hash = request.env['omniauth.auth']
+      first_name, last_name = auth_hash[:info][:name].split(/\s+/, 2)
+      {
+          email: auth_hash[:info][:email],
+          first_name: first_name,
+          last_name: last_name,
+          omniauth: true
+      }
     end
   end
 end
