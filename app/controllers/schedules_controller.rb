8bf45cb96185b980129a8af81b7b72f1c77417b2
Tim Park <timsjpark@gmail.com>
2015-12-02 21:19:10 -0700

    Get rid of extraneous set_employee method
    * Delete extraneous method I copied while drunk on coffee at
      3 AM and forgot to delete later.

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -5,8 +5,4 @@ class SchedulesController < ApplicationController
     @employee = Employee.find(params[:id])
     @shifts = Shift.where(schedule_id: params[:id])
   end
-
-  def set_employee
-    @employee = Employee.find(params[:id])
-  end
 end
