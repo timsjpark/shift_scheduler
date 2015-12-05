12afb8bf080c5c7ed246a33542f6c7824c4b63b3
Tim Park <timsjpark@gmail.com>
2015-12-04 20:19:09 -0700

    Add hacky solution to solving certificate errors

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], {
                scope: ['email', 'https://www.googleapis.com/auth/calendar'],
