1bfcc63b941abec105d89587f18e15940e74a8db
Tim Park <timsjpark@gmail.com>
2015-12-04 18:27:27 -0700

    Modify shift serializer to create custom json view

--- app/serializers/shift_serializer.rb
+++ app/serializers/shift_serializer.rb
@@ -1,3 +1,19 @@
 class ShiftSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :title, :id, :start, :end, :url
+
+  def title
+    object.shift_description
+  end
+
+  def start
+    object.start_time.to_datetime.strftime('%Q')
+  end
+
+  def end
+    object.end_time.to_datetime.strftime('%Q')
+  end
+
+  def url
+    "http://127.0.0.1:3000/shifts/#{object.id}.json"
+  end
 end
