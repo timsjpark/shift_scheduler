222d63a36eb9fc182ba07deeee23609fa0ceb8a2
Tim Park <timsjpark@gmail.com>
2015-12-07 22:45:32 -0700

    This never happened

--- db/migrate/20151202003440_create_shifts.rb
+++ db/migrate/20151202003440_create_shifts.rb
@@ -2,8 +2,8 @@ class CreateShifts < ActiveRecord::Migration
   def change
     create_table :shifts do |t|
       t.string :shift_description
-      t.time :start_time
-      t.time :end_time
+      t.datetime :start_time
+      t.datetime :end_time
       t.belongs_to :schedule, index: true, foreign_key: true
 
       t.timestamps null: false
--- db/migrate/20151202075127_change_date_formats_in_shift.rb
+++
@@ -1,6 +0,0 @@
-class ChangeDateFormatsInShift < ActiveRecord::Migration
-  def change
-    change_column :shifts, :start_time, :datetime
-    change_column :shifts, :end_time, :datetime
-  end
-end
