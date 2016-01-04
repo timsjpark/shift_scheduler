e5fb42d6e6cd8abb4991142bcd15684a84575ea3
Tim Park <timsjpark@gmail.com>
2016-01-03 19:40:10 -0700

    Modify employee factory spec and add manager factory spec

--- spec/factories/managers.rb
+++ spec/factories/managers.rb
@@ -1,12 +1,12 @@
 FactoryGirl.define do
-  factory :employee do
+  factory :manager do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
-    employee_number "1"
+    employee_number { rand(10000) }
     password { Faker::Internet.password}
     password_confirmation { password }
-    type {'Employee'}
+    type {'Manager'}
     organization_id { Faker::Number.between(1, 5)}
     department_id { Faker::Number.between(1, 30)}
   end
