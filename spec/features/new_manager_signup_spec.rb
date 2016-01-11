c9cf1c76f0a80e40c3478adf068b4485ca6167bb
Tim Park <timsjpark@gmail.com>
2016-01-10 19:22:10 -0700

    Create scenario for signing up as a new manager

--- spec/features/new_manager_signup_spec.rb
+++ spec/features/new_manager_signup_spec.rb
@@ -2,6 +2,35 @@ require 'rails_helper'
 
 feature 'Signing up as a manager' do
   scenario 'should allow a new user to sign up as a manager' do
-    
+    manager = FactoryGirl.build(:manager)
+
+    visit '/'
+
+    first('.item').click_link('Signup')
+
+    fill_in 'First Name', with: manager.first_name
+    fill_in 'Last Name', with: manager.last_name
+    fill_in 'Email', with: manager.email
+    fill_in 'Employee Number', with: manager.employee_number
+    fill_in 'Password', with: manager.password
+    fill_in 'Password Confirmation', with: manager.password_confirmation
+
+    click_button 'Signup'
+
+    click_link 'New Organization'
+
+    fill_in 'Name', with: 'SKB'
+
+    click_button 'Create Organization'
+
+    click_link 'Add Department'
+
+    fill_in 'Name', with: 'Sanitation'
+
+    click_button 'Create Department'
+
+    expect(page).to have_content(manager.email)
+    expect(page).to have_content('Sanitation')
+    expect(page).to have_content('Shifts')
   end
 end
